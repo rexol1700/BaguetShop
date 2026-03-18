4c9777e539e48567b0188694dd627595398a0f7c
rexol1700 <janissundt@gmail.com>
2026-03-18 14:07:03 +0100

    Modell

---
+++ modell.js
@@ -0,0 +1,74 @@
+const modell = {
+    app: {
+
+    }, 
+    viewstate: {
+        cake: {
+            taste: null,
+            decore: null,
+            fill: null,
+            message: ""
+        },
+        cart: [],
+        order: {
+            time: null,
+            date: null
+        },
+
+    },
+    data: {
+        category: ["kaffe", "baguett", "iskaffe"],
+        allergies: [{
+            id: 1,
+            name: "nøtter"
+
+        },
+        {
+            id: 2,
+            name: "glucose"
+
+        },
+        {
+            id: 3,
+            name: "lactose"
+
+        }],
+        cake: {
+            taste: ["Sjokolade", "Ostekake", "Bløtkake"],
+            decore: [],
+            fill: []
+        },
+        orders: [{
+            id: null,
+            orderNr: null, 
+            pickup: null,
+            completed: false
+        }],
+        products: [{
+            pris: 20,
+            id: 1,
+            title: "bolle" 
+            allergiesId: [2, 3]
+            availability: 
+            picture: 
+           
+        }
+        
+    
+    
+    ]
+
+    }
+}
+    
+
+allergies[allergiesId
+]
+const BeckaTerminal = {
+    OrderNr: [],
+    bestilling: [],
+    pickup: [],
+    completed: false
+}
+
+console.log(BeckaMeny.meny.matvare[1])
